9a426ed3be50cfeb7fa4dfa8e490ba2aad349af7
Michael Tang <michael.tang02@sap.com>
2020-02-05 15:04:30 -0500

    new board

--- depth-first.py
+++ depth-first.py
@@ -1,40 +1,32 @@
-import numpy as np
+def create_boards():
+    f=open("input-text/initial.txt", "r")
+    contents =f.read()
+    x = contents.split()
+    size = x[0]
+    max_d = x[1]
+    max_l = x[2]
+    numbers = x[3]
+    i=0
+    board = []
 
+    for height in range(int(size)):
+        rows = []
+        for width in range(int(size)):
+            rows.append(int(numbers[i]))
+            i = i+1
+        board.append(rows)
+    return board
 
-def main():
-    f = open("input-text/initial.txt", "r")
-    newArr = []
-    if f.mode == 'r':
-        contents = f.readlines()
-        for x in contents:
-            y = x.split(" ")
-            arr_size = int(y[0])
-            max_d = y[1]
-            for z in y[3]:
-                newArr.append(int(z))
-            #print(newArr)
-            chunks(newArr, arr_size)
-           # print(newArr)
-            newArr2 = np.array_split(newArr, arr_size)
-           # print(newArr2[0][0])
-            for i in range(len(newArr2)):
-                for j in range(len(newArr2[i])):
-                    print(newArr2[i][j], end='')
-                print()
+def success(board):
+    is_successful = True
+    for i in range(len(board)):
+        for j in range(len(board[i])):
+            if(board[i][j] != 0):
+                is_successful = False
+    return is_successful
 
-            # a = np.array(newArr)
-            # a.reshape((arr_size, arr_size))
-            # print(a[1][1])
-
-
-
-def chunks(l, n):
-    n = max(1, n)
-    return (l[i:i + n] for i in range(0, len(l), n))
-
-main()
-
-# 1 0 1
-# 1 1 1
-# 0 0 0
-
+def DFS():
+    open_list = []
+    closed_list = []
+print(create_boards())
+c = [[1, 1, 1, 0], [1, 0, 0, 1], [1, 1, 0, 0], [0, 1, 1, 1]]
