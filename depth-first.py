c8570f95d7497c7dae839ca543bf5b22496b3a93
phistanx <phistan007@gmail.com>
2020-02-13 16:52:40 -0500

    refactor

--- depth-first.py
+++ depth-first.py
@@ -1,7 +1,8 @@
 import copy
 import pickle
 from pathlib import Path
-import re
+import re 
+import functions
 
 class Node:
     state: []
@@ -23,7 +24,7 @@ def play_game():
         search_file = open(search_file_name,"w+")
         solution_file_name = str(index) + "_dfs_solution.txt"
         solution_file = open(solution_file_name,"w+")
-        board = create_boards(line)
+        board = functions.create_boards(line)
         start_dfs(board, get_maxd(line), search_file, solution_file)
         print("====== END GAME ======")
         print("")
@@ -31,23 +32,6 @@ def play_game():
 def get_maxd(contents):
     x = contents.split()
     return int(x[1])
-
-def create_boards(contents):
-    x = contents.split()
-    size = x[0]
-    max_d = x[1]
-    max_l = x[2]
-    numbers = x[3]
-    i = 0
-    board = []
-
-    for height in range(int(size)):
-        rows = []
-        for width in range(int(size)):
-            rows.append(int(numbers[i]))
-            i = i + 1
-        board.append(rows)
-    return board
    
 def start_dfs(initial_board, max_d, search_file, solution_file):
 
@@ -68,15 +52,15 @@ def start_dfs(initial_board, max_d, search_file, solution_file):
     while len(open_stack) > 0:
         current_node = open_stack.pop()
         # add the current node to the search file and append it to the closed stack
-        writeToSearchFile(convertNestedListToString(current_node.state), search_file)
+        functions.writeToSearchFile(functions.convertNestedListToString(current_node.state), search_file)
         closed_stack.append(current_node)
         print(current_node.state)
-        if success(current_node.state):
+        if functions.success(current_node.state):
             no_solution = False
             print("====== SOLUTION ======")
             print(current_node.state)
             print(current_node.coordinates)
-            writeSolutionFile(current_node, solution_file, initial_board)
+            functions.writeSolutionFile(current_node, solution_file, initial_board)
             break
 
         while max_d > current_node.depth:
@@ -84,19 +68,19 @@ def start_dfs(initial_board, max_d, search_file, solution_file):
             if len(open_stack) == 0:
                 break
             current_node = open_stack.pop()
-            writeToSearchFile(convertNestedListToString(current_node.state), search_file)
-            if success(current_node.state):
+            functions.writeToSearchFile(functions.convertNestedListToString(current_node.state), search_file)
+            if functions.success(current_node.state):
                 no_solution = False
                 break
             closed_stack.append(current_node)
             print(current_node.state)
 
-        if success(current_node.state):
+        if functions.success(current_node.state):
             no_solution = False
             print("====== SOLUTION ======")
             print(current_node.state)
             print(current_node.coordinates)
-            writeSolutionFile(current_node,solution_file, initial_board)
+            functions.writeSolutionFile(current_node,solution_file, initial_board)
             break
 
     if no_solution:
@@ -170,17 +154,10 @@ def findChildren(current_node, open_stack, closed_stack):
 
     # compare each node in the list to check if it should go to the open_stack or not
     for node in temp_list:
-        if check_in_closed_stack(node.state, closed_stack):
+        if functions.check_in_closed_stack(node.state, closed_stack):
             open_stack.append(node)
         elif find_depth_in_list(node.state, current_node.depth + 1, closed_stack):
              open_stack.append(node)
-
-def check_in_closed_stack(state, closed_stack):
-    for i in range(len(closed_stack)):
-        if state == closed_stack[i].state:
-            return False
-    return True
-
 
 def find_depth_in_list(state, depth, closed_list):
     try:
@@ -192,46 +169,5 @@ def find_depth_in_list(state, depth, closed_list):
                     return True
     except:
         print('error')
-
-def success(board):
-    is_successful = True
-    for i in range(len(board)):
-        for j in range(len(board[i])):
-            if (board[i][j] != 0):
-                is_successful = False
-    return is_successful
-
-def writeToSearchFile(string_of_list, search_file):
-    search_file.write("0" + " 0 " + "0 " + string_of_list+"\n")
-
-def writeSolutionFile(result_node, solution_file, initial_board):
-    print('PRINT TO FILE')
-    solution_file.write('0 ' + convertNestedListToString(initial_board) + "\n")
-    solution_path = []
-    # append result node and iterate the parents all the way to the root
-    while result_node.parent != None:
-        solution_path.append(result_node)
-        result_node = result_node.parent
-    solution_path.reverse()
-    for i in solution_path:
-        coordinate = convertNestedListToString(convert_coordinate(i.coordinates))
-        state = convertNestedListToString(i.state)
-        solution_file.write(coordinate + " " + state + "\n")
-        print(coordinate, end=' ')
-        print(state)
- 
-def convert_coordinate(coordinate):
-    number = coordinate[0] + 65
-    coordinate[0] = chr(number)
-    return coordinate
-
-def convertNestedListToString(nested_list):
-    string_of_list =  ",".join( repr(e) for e in nested_list)
-    string_of_list = string_of_list.replace(",", "")
-    string_of_list = string_of_list.replace("[","")
-    string_of_list = string_of_list.replace("]","")
-    string_of_list = string_of_list.replace(" ","")
-    string_of_list = string_of_list.replace("'","")
-    return string_of_list
 
 play_game()    
