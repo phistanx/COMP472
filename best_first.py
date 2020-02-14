2b74a84b93e62c331bb2783adf0d55d4af011af9
Michael Tang <michael.tang02@sap.com>
2020-02-14 17:35:36 -0500

    format

--- best_first.py
+++ best_first.py
@@ -1,8 +1,9 @@
 import copy
 import pickle
 from pathlib import Path
-import re 
+import re
 import shared_functions
+
 
 class Node:
     state: []
@@ -11,34 +12,36 @@ class Node:
     coordinates: None
     heuristic: int
 
-    def __init__(self, state, depth, parent, coordinates,heuristic):
+    def __init__(self, state, depth, parent, coordinates, heuristic):
         self.state = state
         self.depth = depth
         self.parent = parent
         self.coordinates = coordinates
         self.heuristic = heuristic
 
+
 def play_game():
     file = open("input-text/initial.txt", "r")
     for index, line in enumerate(file):
         print("====== START GAME ======")
         search_file_name = str(index) + "_bfs_search.txt"
-        search_file = open(search_file_name,"w+")
+        search_file = open(search_file_name, "w+")
         solution_file_name = str(index) + "_bfs_solution.txt"
-        solution_file = open(solution_file_name,"w+")
+        solution_file = open(solution_file_name, "w+")
         board = shared_functions.create_boards(line)
         start_dfs(board, shared_functions.get_maxl(line), search_file, solution_file)
         print("====== END GAME ======")
         print("")
 
+
 def get_maxd(contents):
     x = contents.split()
     return int(x[1])
-   
-def start_dfs(initial_board, max_l, search_file, solution_file):
 
+
+def start_dfs(initial_board, max_l, search_file, solution_file):
     # create Node object containing the state and the depth
-    initial_node = Node(initial_board, 1, None, None,0)
+    initial_node = Node(initial_board, 1, None, None, 0)
 
     # initialize closed and open stack
     open_stack = []
@@ -70,7 +73,8 @@ def start_dfs(initial_board, max_l, search_file, solution_file):
             if len(open_stack) == 0:
                 break
             current_node = open_stack.pop(0)
-            shared_functions.writeToSearchFile(shared_functions.convertNestedListToString(current_node.state), search_file)
+            shared_functions.writeToSearchFile(shared_functions.convertNestedListToString(current_node.state),
+                                               search_file)
             if shared_functions.success(current_node.state):
                 no_solution = False
                 break
@@ -82,12 +86,13 @@ def start_dfs(initial_board, max_l, search_file, solution_file):
             print("====== SOLUTION ======")
             print(current_node.state)
             print(current_node.coordinates)
-            shared_functions.writeSolutionFile(current_node,solution_file, initial_board)
+            shared_functions.writeSolutionFile(current_node, solution_file, initial_board)
             break
 
     if no_solution:
         solution_file.write("No solution")
         print("NO SOLUTION")
+
 
 def findChildren(current_node, open_stack, closed_stack):
     temp_list = []
@@ -141,7 +146,7 @@ def findChildren(current_node, open_stack, closed_stack):
             except:
                 print(end='')
             # we create a Node object containing the values and add it to the list
-            actual_temp_nodes.append(Node(temp_node, current_node.depth + 1, current_node, [i, j+1],0))
+            actual_temp_nodes.append(Node(temp_node, current_node.depth + 1, current_node, [i, j + 1], 0))
 
             heuristic(actual_temp_nodes)
 
@@ -151,28 +156,15 @@ def findChildren(current_node, open_stack, closed_stack):
     # compare each node in the list to check if it should go to the open_stack or not
     for node in actual_temp_nodes:
         if shared_functions.check_in_closed_stack(node.state, closed_stack):
-            open_stack.append(node)      
+            open_stack.append(node)
 
 
-def find_depth_in_list(state, depth, closed_list):
-    try:
-        for i in range(len(closed_list)):
-            if state == closed_list[i].state:
-                if depth < closed_list[i].depth:
-                    closed_list[i].state = state
-                    closed_list[i].depth = depth
-                    return True
-    except:
-        print('error')
-
-
-def heuristic (list_nodes):
+def heuristic(list_nodes):
     for node in list_nodes:
         for i in node.state:
             for j in i:
                 if j == 1:
                     node.heuristic += 1
 
+
 play_game()
-
-
